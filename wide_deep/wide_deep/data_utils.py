c6eb120fde04b4a6b6fcd56fa6bbd518fdbdbc38
mldlx <mldlx@qq.com>
2018-12-27 22:34:17 +0800

    update file

--- wide_deep/wide_deep/data_utils.py
+++ wide_deep/wide_deep/data_utils.py
@@ -6,7 +6,6 @@ from itertools import chain
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-
 pd.options.mode.chained_assignment = None
 
 
@@ -26,7 +25,7 @@ def label_encode(df, cols=None):
     df (pd.Dataframe): mutated df with Label-encoded features.
     """
 
-    if cols == None:
+    if cols is None:
         cols = list(df.select_dtypes(include=['object']).columns)
 
     val_types = dict()
@@ -44,8 +43,7 @@ def label_encode(df, cols=None):
 
 
 def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, target,
-    scale=False, def_dim=8, seed=1981):
-
+                 scale=False, def_dim=8, seed=1981):
     """Prepares a pandas dataframe for the WideDeep model.
 
     Parameters:
@@ -65,8 +63,7 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
     Returns:
     ----------
     wd_dataset (dict): dict with:
-    train_dataset/test_dataset: tuples with the wide, deep and lable training and
-    testing datasets
+    train_dataset/test_dataset: tuples with the wide, deep and lable training and testing datasets
     encoding_dict : dict with useful information about the encoding of the features.
     For example, given a feature 'education' and a value for that feature 'Doctorate'
     encoding_dict['education']['Doctorate'] will return an the encoded integer.
@@ -82,8 +79,8 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
         emb_dim = dict(embeddings_cols)
         embeddings_cols = [emb[0] for emb in embeddings_cols]
     else:
-        emb_dim = {e:def_dim for e in embeddings_cols}
-    deep_cols = embeddings_cols+continuous_cols
+        emb_dim = {e: def_dim for e in embeddings_cols}
+    deep_cols = embeddings_cols + continuous_cols
 
     # Extract the target and copy the dataframe so we don't mutate it
     # internally.
@@ -98,33 +95,33 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
         df_tmp[colname] = df_tmp[cols].apply(lambda x: '-'.join(x), axis=1)
         crossed_columns.append(colname)
 
-    # Extract the categorical column names that can be one hot encoded later
+    #  Extract the categorical column names that can be one hot encoded later
     categorical_columns = list(df_tmp.select_dtypes(include=['object']).columns)
 
     # Encode the dataframe and get the encoding Dictionary only for the
     # deep_cols (for the wide_cols is uneccessary)
-    encoding_dict,df_tmp = label_encode(df_tmp)
-    encoding_dict = {k:encoding_dict[k] for k in encoding_dict if k in deep_cols}
+    encoding_dict, df_tmp = label_encode(df_tmp)
+    encoding_dict = {k: encoding_dict[k] for k in encoding_dict if k in deep_cols}
     embeddings_input = []
-    for k,v in encoding_dict.items():
+    for k, v in encoding_dict.items():
         embeddings_input.append((k, len(v), emb_dim[k]))
 
     # select the deep_cols and get the column index that will be use later
     # to slice the tensors
     df_deep = df_tmp[deep_cols]
-    deep_column_idx = {k:v for v,k in enumerate(df_deep.columns)}
+    deep_column_idx = {k: v for v, k in enumerate(df_deep.columns)}
 
     # The continous columns will be concatenated with the embeddings, so you
     # probably want to normalize them first
     if scale:
         scaler = StandardScaler()
         for cc in continuous_cols:
-            df_deep[cc] = scaler.fit_transform(df_deep[cc].values.reshape(-1,1))
+            df_deep[cc] = scaler.fit_transform(df_deep[cc].values.reshape(-1, 1))
 
     # select the wide_cols and one-hot encode those that are categorical
-    df_wide = df_tmp[wide_cols+crossed_columns]
-    del(df_tmp)
-    dummy_cols = [c for c in wide_cols+crossed_columns if c in categorical_columns]
+    df_wide = df_tmp[wide_cols + crossed_columns]
+    del (df_tmp)
+    dummy_cols = [c for c in wide_cols + crossed_columns if c in categorical_columns]
     df_wide = pd.get_dummies(df_wide, columns=dummy_cols)
 
     # train/test split
@@ -135,13 +132,11 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
     # Building the output dictionary
     wd_dataset = dict()
     train_dataset = namedtuple('train_dataset', 'wide, deep, labels')
-    test_dataset  = namedtuple('test_dataset' , 'wide, deep, labels')
+    test_dataset = namedtuple('test_dataset', 'wide, deep, labels')
     wd_dataset['train_dataset'] = train_dataset(X_train_wide, X_train_deep, y_train)
-    wd_dataset['test_dataset']  = test_dataset(X_test_wide, X_test_deep, y_test)
-    wd_dataset['embeddings_input']  = embeddings_input
+    wd_dataset['test_dataset'] = test_dataset(X_test_wide, X_test_deep, y_test)
+    wd_dataset['embeddings_input'] = embeddings_input
     wd_dataset['deep_column_idx'] = deep_column_idx
     wd_dataset['encoding_dict'] = encoding_dict
 
     return wd_dataset
-
-
--- wide_deep/wide_deep/torch_model.py
+++ wide_deep/wide_deep/torch_model.py
@@ -8,7 +8,6 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 
-
 use_cuda = torch.cuda.is_available()
 
 
@@ -19,17 +18,16 @@ class WideDeepLoader(Dataset):
     --------
     data: namedtuple with 3 elements - (wide_input_data, deep_inp_data, target)
     """
-    def __init__(self, data):
 
+    def __init__(self, data):
         self.X_wide = data.wide
         self.X_deep = data.deep
         self.Y = data.labels
 
     def __getitem__(self, idx):
-
         xw = self.X_wide[idx]
         xd = self.X_deep[idx]
-        y  = self.Y[idx]
+        y = self.Y[idx]
 
         return xw, xd, y
 
@@ -79,22 +77,21 @@ class WideDeep(nn.Module):
         self.n_class = n_class
 
         # Build the embedding layers to be passed through the deep-side
-        for col,val,dim in self.embeddings_input:
-            setattr(self, 'emb_layer_'+col, nn.Embedding(val, dim))
+        for col, val, dim in self.embeddings_input:
+            setattr(self, 'emb_layer_' + col, nn.Embedding(val, dim))
 
         # Build the deep-side hidden layers with dropout if specified
         input_emb_dim = np.sum([emb[2] for emb in self.embeddings_input])
-        self.linear_1 = nn.Linear(input_emb_dim+len(continuous_cols), self.hidden_layers[0])
+        self.linear_1 = nn.Linear(input_emb_dim + len(continuous_cols), self.hidden_layers[0])
         if self.dropout:
             self.linear_1_drop = nn.Dropout(self.dropout[0])
-        for i,h in enumerate(self.hidden_layers[1:],1):
-            setattr(self, 'linear_'+str(i+1), nn.Linear( self.hidden_layers[i-1], self.hidden_layers[i] ))
+        for i, h in enumerate(self.hidden_layers[1:], 1):
+            setattr(self, 'linear_' + str(i + 1), nn.Linear(self.hidden_layers[i - 1], self.hidden_layers[i]))
             if self.dropout:
-                setattr(self, 'linear_'+str(i+1)+'_drop', nn.Dropout(self.dropout[i]))
+                setattr(self, 'linear_' + str(i + 1) + '_drop', nn.Dropout(self.dropout[i]))
 
         # Connect the wide- and dee-side of the model to the output neuron(s)
-        self.output = nn.Linear(self.hidden_layers[-1]+self.wide_dim, self.n_class)
-
+        self.output = nn.Linear(self.hidden_layers[-1] + self.wide_dim, self.n_class)
 
     def compile(self, method="logistic", optimizer="Adam", learning_rate=0.001, momentum=0.0):
         """Wrapper to set the activation, loss and the optimizer.
@@ -120,7 +117,6 @@ class WideDeep(nn.Module):
 
         self.method = method
 
-
     def forward(self, X_w, X_d):
         """Implementation of the forward pass.
 
@@ -134,22 +130,22 @@ class WideDeep(nn.Module):
         out (torch.tensor) : result of the output neuron(s)
         """
         # Deep Side
-        emb = [getattr(self, 'emb_layer_'+col)(X_d[:,self.deep_column_idx[col]].long())
-               for col,_,_ in self.embeddings_input]
+        emb = [getattr(self, 'emb_layer_' + col)(X_d[:, self.deep_column_idx[col]].long())
+               for col, _, _ in self.embeddings_input]
         if self.continuous_cols:
             cont_idx = [self.deep_column_idx[col] for col in self.continuous_cols]
             cont = [X_d[:, cont_idx].float()]
-            deep_inp = torch.cat(emb+cont, 1)
+            deep_inp = torch.cat(emb + cont, 1)
         else:
             deep_inp = torch.cat(emb, 1)
 
         x_deep = F.relu(self.linear_1(deep_inp))
         if self.dropout:
             x_deep = self.linear_1_drop(x_deep)
-        for i in range(1,len(self.hidden_layers)):
-            x_deep = F.relu( getattr(self, 'linear_'+str(i+1))(x_deep) )
+        for i in range(1, len(self.hidden_layers)):
+            x_deep = F.relu(getattr(self, 'linear_' + str(i + 1))(x_deep))
             if self.dropout:
-                x_deep = getattr(self, 'linear_'+str(i+1)+'_drop')(x_deep)
+                x_deep = getattr(self, 'linear_' + str(i + 1) + '_drop')(x_deep)
 
         # Deep + Wide sides
         wide_deep_input = torch.cat([x_deep, X_w.float()], 1)
@@ -160,7 +156,6 @@ class WideDeep(nn.Module):
             out = self.activation(self.output(wide_deep_input))
 
         return out
-
 
     def fit(self, dataset, n_epochs, batch_size):
         """Run the model for the training set at dataset.
@@ -176,12 +171,11 @@ class WideDeep(nn.Module):
         train_loader = torch.utils.data.DataLoader(dataset=widedeep_dataset,
                                                    batch_size=batch_size,
                                                    shuffle=True)
-
         # set the model in training mode
         net = self.train()
         for epoch in range(n_epochs):
-            total=0
-            correct=0
+            total = 0
+            correct = 0
             for i, (X_wide, X_deep, target) in enumerate(train_loader):
                 X_w = Variable(X_wide)
                 X_d = Variable(X_deep)
@@ -191,26 +185,26 @@ class WideDeep(nn.Module):
                     X_w, X_d, y = X_w.cuda(), X_d.cuda(), y.cuda()
 
                 self.optimizer.zero_grad()
-                y_pred =  net(X_w, X_d)
+                y_pred = net(X_w, X_d)
                 loss = self.criterion(y_pred, y)
                 loss.backward()
                 self.optimizer.step()
 
                 if self.method != "regression":
-                    total+= y.size(0)
+                    total += y.size(0)
                     if self.method == 'logistic':
                         y_pred_cat = (y_pred > 0.5).squeeze(1).float()
                     if self.method == "multiclass":
                         _, y_pred_cat = torch.max(y_pred, 1)
-                    correct+= float((y_pred_cat == y).sum().data[0])
+                    correct += float((y_pred_cat == y).sum().data[0])
 
             if self.method != "regression":
-                print ('Epoch {} of {}, Loss: {}, accuracy: {}'.format(epoch+1,
-                    n_epochs, round(loss.data[0],3), round(correct/total,4)))
+                print('Epoch {} of {}, Loss: {}, accuracy: {}'.format(epoch + 1,
+                                                                      n_epochs, round(loss.data[0], 3),
+                                                                      round(correct / total, 4)))
             else:
-                print ('Epoch {} of {}, Loss: {}'.format(epoch+1, n_epochs,
-                    round(loss.data[0],3)))
-
+                print('Epoch {} of {}, Loss: {}'.format(epoch + 1, n_epochs,
+                                                        round(loss.data[0], 3)))
 
     def predict(self, dataset):
         """Predict target for dataset.
@@ -233,7 +227,7 @@ class WideDeep(nn.Module):
 
         # set the model in evaluation mode so dropout is not applied
         net = self.eval()
-        pred = net(X_w,X_d).cpu()
+        pred = net(X_w, X_d).cpu()
         if self.method == "regression":
             return pred.squeeze(1).data.numpy()
         if self.method == "logistic":
@@ -241,7 +235,6 @@ class WideDeep(nn.Module):
         if self.method == "multiclass":
             _, pred_cat = torch.max(pred, 1)
             return pred_cat.data.numpy()
-
 
     def predict_proba(self, dataset):
         """Predict predict probability for dataset.
@@ -265,16 +258,15 @@ class WideDeep(nn.Module):
 
         # set the model in evaluation mode so dropout is not applied
         net = self.eval()
-        pred = net(X_w,X_d).cpu()
+        pred = net(X_w, X_d).cpu()
         if self.method == "logistic":
             pred = pred.squeeze(1).data.numpy()
-            probs = np.zeros([pred.shape[0],2])
-            probs[:,0] = 1-pred
-            probs[:,1] = pred
+            probs = np.zeros([pred.shape[0], 2])
+            probs[:, 0] = 1 - pred
+            probs[:, 1] = pred
             return probs
         if self.method == "multiclass":
             return pred.data.numpy()
-
 
     def get_embeddings(self, col_name):
         """Extract the embeddings for the embedding columns.
@@ -289,14 +281,12 @@ class WideDeep(nn.Module):
 
         params = list(self.named_parameters())
         emb_layers = [p for p in params if 'emb_layer' in p[0]]
-        emb_layer  = [layer for layer in emb_layers if col_name in layer[0]][0]
+        emb_layer = [layer for layer in emb_layers if col_name in layer[0]][0]
         embeddings = emb_layer[1].cpu().data.numpy()
         col_label_encoding = self.encoding_dict[col_name]
-        inv_dict = {v:k for k,v in col_label_encoding.iteritems()}
+        inv_dict = {v: k for k, v in col_label_encoding.iteritems()}
         embeddings_dict = {}
-        for idx,value in inv_dict.items():
+        for idx, value in inv_dict.items():
             embeddings_dict[value] = embeddings[idx]
 
         return embeddings_dict
-
-
